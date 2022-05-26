96ba78336cea90e5882048015ff2a30557d28a1d
Barak_NK <barakuch28032004@gmail.com>
2022-05-26 05:03:34 +0300

    Necessary global variables declaration

--- snake.py
+++ snake.py
@@ -1,6 +1,36 @@
 import turtle
 import random
 
+turtle.bgcolor("black")
+turtle.tracer(1,0) #This helps the turtle move more smoothly
+turtle.penup()
+turtle.hideturtle()
+
+SIZE_X=800
+SIZE_Y=500
+turtle.setup(SIZE_X, SIZE_Y)
+
+UP_EDGE = 250
+DOWN_EDGE = -250
+RIGHT_EDGE = 400
+LEFT_EDGE = -400
+
+SQUARE_SIZE = 20
+START_LENGTH = 8
+TIME_STEP = 100
+
+#Initialize lists
+pos_list = []
+stamp_list = []
+food_pos = []
+food_stamps = []
+
+#Creating the snake
+snake = turtle.clone()
+snake.shape("square")
+snake.color("turquoise")
+snake.direction = "Up"
+
 def main():
     pass
 
